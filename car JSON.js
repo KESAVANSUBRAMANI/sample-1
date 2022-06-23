4dc004b4885c11826eadd287acbae221e90073be
KESAVANSUBRAMANI <101824593+KESAVANSUBRAMANI@users.noreply.github.com>
2022-06-23 09:13:49 +0530

    Add files via upload

---
+++ car JSON.js
@@ -0,0 +1,17 @@
+var car = {
+    'dimensions':{
+     "overall length" : 3500,
+     "overall width" : 1600,
+     "overall height": 1490,
+     "wheelbase"     :2360,
+     "track width front": 1405,
+     "trackwidth back" : 1400,
+     "minimum turning radius": 4.5,
+     "minimum ground clearance":170
+},
+    'capacity': {
+        "seating capacity" : "5 persons",
+        "fuel tank capacity" : "35 litres"
+    }
+}
+console.log(car.capacity);
